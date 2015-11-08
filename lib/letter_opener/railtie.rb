5d5ab2eddcd1167bed00f8380e1d72129a6bca42
mamluka <david.mazvovsky@gmail.com>
2015-11-08 15:08:00 +0200

    add a test method that doesn't open browser window

--- lib/letter_opener/railtie.rb
+++ lib/letter_opener/railtie.rb
@@ -2,7 +2,13 @@ module LetterOpener
   class Railtie < Rails::Railtie
     initializer "letter_opener.add_delivery_method" do
       ActiveSupport.on_load :action_mailer do
-        ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, :location => Rails.root.join("tmp", "letter_opener")
+        ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, :location => Rails.root.join("tmp", "letter_opener"), preview_in_browser: true
+      end
+    end
+
+    initializer "letter_opener_test.add_delivery_method" do
+      ActiveSupport.on_load :action_mailer do
+        ActionMailer::Base.add_delivery_method :letter_opener_test, LetterOpener::DeliveryMethod, :location => Rails.root.join("tmp", "letter_opener"), preview_in_browser: false
       end
     end
   end
